f2f529622fde02d02dad332ad9f3931704682653
DJStefan <Stefan4472@users.noreply.github.com>
2015-09-13 11:13:14 -0400

    Initial Commit
    Copy and Pasted from the TextManipulator, with a few changes. There is obviously a lot of work to be done.

---
+++ TextFile.java
@@ -0,0 +1,122 @@
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/* Class that handles textfiles, files that can be read and written
+ * with Strings */
+public class TextFile extends File {
+
+  /* Constructor using path as a String */
+  public TextFile(String path) {
+      super(path);
+  }
+
+  /* Returns whether this file exists as a file and can be read */
+  public boolean isValid() {
+      return isFile() && canRead();
+  }
+
+  /* Writes file by "expanding" the array into a single String
+   * and writing that String
+   * @param text String array to write to file */
+  public void writeFile(String[] text) {
+    String file_text = "";
+    for(int i = 0; i < text.length; i++)
+        file_text += text[i];
+    writeFile(file_text);
+  }
+
+  /* Appends String to file
+   * @param append String to add to the  end of the file */
+  public void appendText(String append) {
+    writeFile(readFile() + append);
+  }
+
+  /* Removes all text from the file */
+  public void clear() {
+      writeFile("");
+  }
+
+  /* Reads file and returns file contents as String
+   * @return file contents, null if file could not be read */
+  public String readFile() {
+      /* Try creating a BufferedReader using the file's path */
+      try (BufferedReader reader = Files.newBufferedReader(Paths.get(getPath()))) {
+          String line;
+          String text = "";
+          int line_counter = 0;
+          while ((line = reader.readLine()) != null) {
+              if(line_counter == 0)
+                  text = line;
+              else
+                  text += "\n" + line;
+              line_counter++;
+          }
+          return text;
+    } catch (IOException e) {
+          return null;
+    }
+  }
+  /* Writes file with String
+   * @param file_text String to write to the file */
+  public boolean writeFile(String file_text) {
+    /* Try creating a BufferedWriter using the file's path */
+    try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(getPath()))) {
+      writer.write(file_text, 0, file_text.length());
+      return true;
+    } catch (IOException e) {
+      return false;
+    }
+  }
+
+  /* Deletes file */
+  public boolean delete() {
+    return delete();
+  }
+
+  /* Reads file line by line and returns an arrayList of lines
+   * @return an arrayList containing filetext as lines, or null if file
+    * couldn't be read */
+  public ArrayList<String> readLines() {
+      ArrayList<String> file_lines = new ArrayList<String> ();
+      try {
+          BufferedReader reader = new BufferedReader(new FileReader(this));
+          String line;
+          while ((line = reader.readLine()) != null) {
+              file_lines.add(line);
+          }
+          return file_lines;
+      } catch(IOException e) {
+          return null;
+      }
+  }
+
+  /* Pastes clipboard contents into file
+   * @return whether clipboard contents were accessed and written successfully */
+  public boolean pasteIntoFile() { // Todo: fix bug where linebreaks are lost
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        /* get contents from clipboard (stored in a Transferable, which manages data transfer) */
+        Transferable contents = clipboard.getContents(null);
+        /* if contents are transferable, the Transferable will not be null and will be the
+        correct DataFlavor (String). DataFlavor refers to the type of object something is */
+        boolean hasTransferableText =
+                (contents != null) && contents.isDataFlavorSupported(DataFlavor.stringFlavor);
+        if (hasTransferableText) {
+            try {
+                return writeFile((String)contents.getTransferData(DataFlavor.stringFlavor));
+            } catch (UnsupportedFlavorException|IOException e) {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+}
