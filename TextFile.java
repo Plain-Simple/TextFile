15572a58311dab99496555218bdfa0106a2d3720
stefan4472 <stefan.kussmaul@gmail.com>
2015-09-25 22:19:58 -0400

    Added License and Header

--- TextFile.java
+++ TextFile.java
@@ -7,13 +7,24 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
- * Class that handles textfiles, files that can be read and written
- * with Strings.
- *
- * PlainSimple 2015. See github.com/Plain-Simple/TextFile for more
- * information.
- *
+ * A java utility class for handling simple textfiles.
+ * Copyright (C) Plain Simple Apps
  * @author Stefan Kussmaul
+ * 
+ * See <https://github.com/Plain-Simple/TextFile> for more information.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 public class TextFile extends File {
 
